86a5f478a2ed6eba02286eb94990360c2f09a883
eleo-ongaro <eleo.ongaro@gmail.com>
2023-07-16 14:29:58 -0300

    funciones de fitro añadidas - funcionando

--- main.js
+++ main.js
@@ -12,30 +12,42 @@ const changeBackground = (e) => {
     imgMeme.style.backgroundImage = `url('${e.target.value}')`
     imgMeme.style.backgroundSize = 'cover'
     imgMeme.style.backgroundRepeat = 'no-repeat'
-    imgMeme.style.backgroundRepeat = 'center'
+    imgMeme.style.backgroundPosition = 'center'
 }
 
 // FILTROS IMG
 
 const brightFilter = document.getElementById('bright-filter');
-
-urlInput.addEventListener('input', (e)=> changeBright(e))
-
-const changeBright = (e) => {
-    imgMeme.style.backgroundImage = `url('${e.target.value}')`
-    imgMeme.style.backgroundSize = 'cover'
-    imgMeme.style.backgroundRepeat = 'no-repeat'
-    imgMeme.style.backgroundRepeat = 'center'
-}
-
+brightFilter.addEventListener('input', () => filtros());
+//console.log(brightFilter);
 const opFilter = document.getElementById('op-filter');
+opFilter.addEventListener('input', () => filtros());
+// console.log(opFilter.value);
 const contrastFilter = document.getElementById('contrast-filter');
+contrastFilter.addEventListener('input', () => filtros());
+// console.log(contrastFilter.value);
 const blurFilter = document.getElementById('blur-filter');
+blurFilter.addEventListener('input', () => filtros());
+// console.log(blurFilter.value);
 const grayScaleFilter = document.getElementById('gray-scale-filter');
+grayScaleFilter.addEventListener('input', () => filtros());
+// console.log(grayScaleFilter.value);
 const sepiaFilter = document.getElementById('sepia-filter');
+sepiaFilter.addEventListener('input', () => filtros());
+// console.log(sepiaFilter.value);
 const satFilter = document.getElementById('sat-filter');
+satFilter.addEventListener('input', () => filtros());
+// console.log(satFilter.value);
+const hueFilter = document.getElementById('hue-filter');
+hueFilter.addEventListener('input', () => filtros());
+// console.log(satFilter.value);
 const negativeFilter = document.getElementById('negative-filter');
+negativeFilter.addEventListener('input', () => filtros());
+// console.log(negativeFilter.value);
 
+const filtros = () => {
+    imgMeme.style.filter = `brightness(${brightFilter.value}) opacity(${opFilter.value}) blur(${blurFilter.value}px) contrast(${contrastFilter.value}%) grayscale(${grayScaleFilter.value}%) hue-rotate(${hueFilter.value}deg) sepia(${sepiaFilter.value}%) saturate(${satFilter.value}%) invert(${negativeFilter.value})`;
+}
 
 
 
